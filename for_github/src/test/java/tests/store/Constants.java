c897470f6bd4243b3ad471335c83931ac185bc29
bogfaust <73497633+bogfaust@users.noreply.github.com>
2023-03-03 19:41:37 +0400

    Delete for_github directory

--- for_github/src/test/java/tests/store/Constants.java
+++
@@ -1,6 +0,0 @@
-package tests.store;
-
-public class Constants {
-	public static final String CONFIG_FILE_PATH = "data/config.json";
-	public static final String TEST_DATA_FILE_PATH = "data/test_data.json";
-}
